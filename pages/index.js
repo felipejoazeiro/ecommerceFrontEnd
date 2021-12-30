74537f08a4175eeb9a2148406486d16aaae2fb94
felipejoazeiro <felipejoazeiro@gmail.com>
2021-12-29 22:33:17 -0300

    Configuração do HEAD

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,22 @@ import React, {Component} from "react";
 
 import Layout from '../components/Layout'
 
-//import Cabecalho from '../containers/Cabecalho'
-//import Banners from '../containers/Banners'
-//import Beneficios from '../containers/Beneficios'
-//import ProdutosPaginaInicial from '../containers/Lista/ProdutosPaginaInicial'
-//import Rodape from '../containers/Rodape'
+import Cabecalho from '../containers/Cabecalho'
+import Banners from '../containers/Banners'
+import Beneficios from '../containers/Beneficios'
+import ProdutosPaginaInicial from '../containers/Lista/ProdutosPaginaInicial'
+import Rodape from '../containers/Rodape'
 
 export default class Index extends Component {
     render(){
         return(
             <Layout title="LOJA IT - Melhores produtos de informática">
-                <h2>Loja IT</h2>
+                <Cabecalho simples/>
+                <Banners />
+                <Beneficios />
+                <ProdutosPaginaInicial />
+                <Rodape />
             </Layout>
-
         )
     }
 }
