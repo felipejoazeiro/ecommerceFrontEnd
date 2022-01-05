4f88bea0e2cfd067a8806a447b753a215510e7bb
felipejoazeiro <felipejoazeiro@gmail.com>
2022-01-05 18:51:40 -0300

    Criacao das paginas da area de cliente

--- pages/sucesso.js
+++ pages/sucesso.js
@@ -1,13 +1,18 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import SucessoContainer from '../containers/Sucesso';
+import Rodape from '../containers/Rodape';
 
 export default class Sucesso extends Component {
     render(){
         return(
-            <div>
-                <h2>Loja - SUCESSO</h2>
-                
-            </div>
-
+            <Layout title="LOJA IT - Melhores produtos em Informática">
+                <Cabecalho simples />
+                <SucessoContainer />
+                <Rodape />
+            </Layout>
         )
     }
 }
